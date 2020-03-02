cd1f8bd6d8c1309bcd78e52b8a3665429f5ce753
Darryl Kilzer <darrylkilzer@gmail.com>
2020-03-02 10:15:39 -0700

    fix localhost in store to default the port

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -6,7 +6,7 @@ import router from '../router/index'
 Vue.use(Vuex)
 
 //Allows axios to work locally or live
-let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
+let base = window.location.host.includes('localhost') ? '//localhost:3000/' : '/'
 
 let api = Axios.create({
   baseURL: base + "api/",
