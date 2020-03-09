d6b36504ba9479332728819b5661e680edef8838
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-03-09 12:51:50 -0600

    resolves redirect

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link class="navbar-brand" :to="{ name: 'home' }">Kanban</router-link>
+    <router-link class="navbar-brand" :to="{ name: 'home' }"
+      >Kanban</router-link
+    >
     <button
       class="navbar-toggler"
       type="button"
@@ -15,18 +17,28 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
-          <router-link :to="{ name: 'home' }" class="nav-link">Home</router-link>
+          <router-link :to="{ name: 'home' }" class="nav-link"
+            >Home</router-link
+          >
         </li>
         <li
           class="nav-item"
           v-if="$auth.isAuthenticated"
           :class="{ active: $route.name == 'boards' }"
         >
-          <router-link class="nav-link" :to="{ name: 'boards' }">My-Dashboard</router-link>
+          <router-link class="nav-link" :to="{ name: 'boards' }"
+            >My-Dashboard</router-link
+          >
         </li>
       </ul>
       <span class="navbar-text">
-        <button class="btn btn-success" @click="login" v-if="!$auth.isAuthenticated">Login</button>
+        <button
+          class="btn btn-success"
+          @click="login"
+          v-if="!$auth.isAuthenticated"
+        >
+          Login
+        </button>
         <button class="btn btn-danger" @click="logout" v-else>logout</button>
       </span>
     </div>
@@ -51,9 +63,8 @@ export default {
       console.log(this.$auth.user);
     },
     async logout() {
-      await this.$auth.logout();
       this.$store.dispatch("resetBearer");
-      this.$router.push({ name: "home" });
+      await this.$auth.logout(window.location.origin);
     }
   }
 };
