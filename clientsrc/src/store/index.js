add7a06e4b7ca1175a28658a981efdc82574dfb4
Tim Dohmen <dohmentim@yahoo.com>
2020-08-07 09:46:53 -0600

    axiosService

--- clientsrc/src/store/index.js
+++ clientsrc/src/store/index.js
@@ -1,18 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Axios from 'axios'
 import router from '../router/index'
+import { api } from "./AxiosService"
 
 Vue.use(Vuex)
 
-//Allows axios to work locally or live
-let base = window.location.host.includes('localhost') ? '//localhost:3000/' : '/'
 
-let api = Axios.create({
-  baseURL: base + "api/",
-  timeout: 3000,
-  withCredentials: true
-})
 
 export default new Vuex.Store({
   state: {
